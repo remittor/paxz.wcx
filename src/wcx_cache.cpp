680fd69aee6d558ddff6a4c82ca4bb7c9ac0745f
remittor <remittor@gmail.com>
2019-11-29 17:54:33 +0300

    Fix zero terminating error when using MultiByteToWideChar

--- src/wcx_cache.cpp
+++ src/wcx_cache.cpp
@@ -311,6 +311,7 @@ int cache::add_pax_info(tar::pax_decode & pax, UINT64 file_pos, UINT64 frame_siz
           break;
         int x = MultiByteToWideChar(CP_UTF8, 0, name, (int)nlen, m_add_name.data(), (int)m_add_name.capacity() - 32);
         FIN_IF(x <= 0, 0x45013400 | E_EOPEN);
+        m_add_name.data()[x] = 0;
         m_add_name.fix_length();
         LPWSTR ws = m_add_name.data();
         do {
@@ -483,7 +484,7 @@ int cache::scan_paxlz4_file(HANDLE hFile)
     size_t plen = frame.data_size;
     DWORD calc_hash = XXH32(pd, plen, 0);
     DWORD read_hash = *(PDWORD)(pd + plen);
-    LOGi("pos = 0x%X  hash = %08X  cacl = %08X ", (DWORD)pos.QuadPart - 4, read_hash, calc_hash);
+    //LOGi("pos = 0x%X  hash = %08X  calc = %08X ", (DWORD)pos.QuadPart - 4, read_hash, calc_hash);
     FIN_IF(read_hash != calc_hash, 0x44013700 | E_EOPEN);
 
     if (frame.is_compressed) {
