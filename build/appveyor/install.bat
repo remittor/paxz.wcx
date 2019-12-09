89b3e2098593eecdd12eaf7ea9c7a7f872732aed
remittor <remittor@gmail.com>
2019-12-09 16:24:58 +0300

    Download LZ4 and Zstd projects as submodules

--- build/appveyor/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set "EXT_LIBS=%APPVEYOR_BUILD_FOLDER%\extlibs"
-
-ECHO Install LZ4 project ...
-git clone --depth 1 --branch=master https://github.com/lz4/lz4.git %EXT_LIBS%\lz4
-
-ECHO Install Zstd project ...
-git clone --depth 1 --branch=master https://github.com/facebook/zstd.git %EXT_LIBS%\zstd
